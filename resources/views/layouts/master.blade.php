28f36fa93ab66985a0de1b144bd64da166d31570
IswahyuddinSuhrillah <108008352+IswahyuddinSuhrillah@users.noreply.github.com>
2022-07-20 19:56:13 +0700

    Pembuatan Controller & Route

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -111,7 +111,7 @@
         <div class="float-right d-none d-sm-block">
         <b>Version</b> 3.2.0
         </div>
-        <strong>Copyright &copy; 2022 <a href="/">IS-058</a>.</strong> All rights reserved.
+        <strong>Copyright &copy; 2022 <a href="/">Iswahyuddin Suhrillah</a>.</strong> All rights reserved.
     </footer>
     </div>
     <!-- ./wrapper -->
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JadwalController;
+use App\Http\Controllers\LapanganController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +18,22 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('layouts.master');
 // });
+
+//Lapangan
+Route::get('/lapangan',[LapanganController::class, 'index']);
+Route::get('/lapangan/form',[LapanganController::class, 'create']);
+Route::post('/lapangan/store',[LapanganController::class, 'store']);
+Route::get('/lapangan/edit/{id}',[LapanganController::class, 'edit']);
+Route::put('/lapangan/{id}',[LapanganController::class, 'update']);
+Route::delete('/lapangan/{id}',[LapanganController::class, 'destroy']);
+
+//Sewa
+Route::get('/sewa',[SewaController::class, 'index']);
+Route::get('/sewa/form',[SewaController::class, 'create']);
+Route::post('/sewa/store',[SewaController::class, 'store']);
+Route::get('/sewa/edit/{id}',[SewaController::class, 'edit']);
+Route::put('/sewa/{id}',[SewaController::class, 'update']);
+Route::delete('/sewa/{id}',[SewaController::class, 'destroy']);
 
 Auth::routes();
 
